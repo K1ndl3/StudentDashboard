7d98d1779ce9aa8c4132c65a898adade34ae2b1a
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-12-29 17:54:55 -0800

    finished log in page and routing for front end. Need to start on backend for token verification

--- frontEnd/studentDashboard_frontEnd/src/App.jsx
+++ frontEnd/studentDashboard_frontEnd/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './components/login/login'
+import { BrowserRouter, Routes, Route, useNavigate, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Login></Login>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- frontEnd/studentDashboard_frontEnd/src/components/login/login.jsx
+++ frontEnd/studentDashboard_frontEnd/src/components/login/login.jsx
@@ -1,8 +1,50 @@
+import { useNavigate } from "react-router-dom"
+import "./login.css"
+import { useState } from "react"
+
 
 function Login() {
+    const [token, setToken] = useState("")
+    const navigate = useNavigate()
+
+    const logInSubmit = async () =>{
+        try {
+        const res = await fetch("auth/dev-token", {
+            method : "Post",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({token})
+        })
+
+        if (res.ok) {
+            navigate("/dashboard")
+        } else {
+            alert("Invalid Token. Try again")
+            } 
+        }catch (err) {
+      console.error(err);
+      alert("Network error!");
+        }
+    }
+    
     return (
         <div className="login-container">
-            
+            <div className="text">
+                <h3 className="login-title">Scholar Sync</h3>
+                <h1>Welcome</h1>
+            </div>
+            <div className="button-bar">
+                <input className="input"
+                    type="password"
+                    value={token}
+                    placeholder="Canvas Developer Token"
+                    onChange={e => setToken(e.target.value)}>
+                </input>
+                <button className="login-button"
+                    onClick={logInSubmit}
+                >
+                    Log in
+                </button>
+            </div>
         </div>
     )
 }
