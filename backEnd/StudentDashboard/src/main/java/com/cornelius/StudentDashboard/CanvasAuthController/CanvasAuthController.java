6372b935f879d3a830dad757d7c4c24feda8c962
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2026-01-20 13:13:58 -0800

    finished refactoring dev-auth into canvas client

--- backEnd/StudentDashboard/src/main/java/com/cornelius/StudentDashboard/CanvasAuthController/CanvasAuthController.java
+++ backEnd/StudentDashboard/src/main/java/com/cornelius/StudentDashboard/CanvasAuthController/CanvasAuthController.java
@@ -1,38 +1,38 @@
-package com.cornelius.StudentDashboard.CanvasAuthController;
+    package com.cornelius.StudentDashboard.CanvasAuthController;
 
-import com.cornelius.StudentDashboard.CanvasAuthService.CanvasAuthService;
-import com.cornelius.StudentDashboard.Exception.CanvasUnavailableException;
-import com.cornelius.StudentDashboard.Exception.InsufficientScopeException;
-import com.cornelius.StudentDashboard.Exception.InvalidTokenException;
-import com.cornelius.StudentDashboard.dto.auth.DevTokenRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+    import com.cornelius.StudentDashboard.CanvasAuthService.CanvasAuthService;
+    import com.cornelius.StudentDashboard.Exception.CanvasUnavailableException;
+    import com.cornelius.StudentDashboard.Exception.InsufficientScopeException;
+    import com.cornelius.StudentDashboard.Exception.InvalidTokenException;
+    import com.cornelius.StudentDashboard.dto.auth.DevTokenRequest;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth")
-public class CanvasAuthController {
-    private CanvasAuthService authService;
-    public CanvasAuthController(CanvasAuthService service) {
-        this.authService = service;
-    }
-
-    @PostMapping("/dev-token")
-    public ResponseEntity<Void> validateDevToken(@RequestBody DevTokenRequest dto) {
-        String token = dto.getToken();
-        try {
-            authService.validateDevToken(token);
-            return ResponseEntity.noContent().build();
-        } catch (InvalidTokenException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        } catch (InsufficientScopeException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        } catch (CanvasUnavailableException e) {
-            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).build();
+    @RestController
+    @RequestMapping("/auth")
+    public class CanvasAuthController {
+        private CanvasAuthService authService;
+        public CanvasAuthController(CanvasAuthService service) {
+            this.authService = service;
         }
 
+        @PostMapping("/dev-token")
+        public ResponseEntity<Void> validateDevToken(@RequestBody DevTokenRequest dto) {
+            String token = dto.getToken();
+            try {
+                authService.validateDevToken(token);
+                return ResponseEntity.noContent().build();
+            } catch (InvalidTokenException e) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            } catch (InsufficientScopeException e) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            } catch (CanvasUnavailableException e) {
+                return ResponseEntity.status(HttpStatus.BAD_GATEWAY).build();
+            }
+
+        }
     }
-}
---
+++ backEnd/StudentDashboard/src/main/java/com/cornelius/StudentDashboard/CanvasClient/CanvasClient.java
@@ -0,0 +1,40 @@
+package com.cornelius.StudentDashboard.CanvasClient;
+
+import com.cornelius.StudentDashboard.Exception.CanvasUnavailableException;
+import com.cornelius.StudentDashboard.Exception.InsufficientScopeException;
+import com.cornelius.StudentDashboard.Exception.InvalidTokenException;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CanvasClient {
+    final private String BASE_URL_CSUF = "https://csufullerton.instructure.com/api/";
+
+    public void ValidateDevToken(String token) {
+        String endpoint = "v1/users/self";
+        String url = BASE_URL_CSUF + endpoint;
+
+        HttpHeaders header = new HttpHeaders();
+        header.setBearerAuth(token);
+        RestTemplate rt = new RestTemplate();
+        HttpEntity<Void> entity = new HttpEntity<>(header);
+
+        try {
+            ResponseEntity<String> response = rt.exchange(url, HttpMethod.GET, entity, String.class);
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                throw new InvalidTokenException();
+            }
+            if (e.getStatusCode() == HttpStatus.FORBIDDEN) {
+                throw new InsufficientScopeException("Under Leveled Privilege");
+            }
+            throw e;
+        } catch (HttpServerErrorException | ResourceAccessException e) {
+            throw new CanvasUnavailableException("Canvas in unvailable");
+        }
+    }
+}
