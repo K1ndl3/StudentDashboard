6cb7d60c7d9d827749796d2bf692a22d0fb05a0d
Huy Bui <HUYBUI02@csu.fullerton.edu>
2026-01-27 13:37:39 -0800

    added service layer for canvas course info

---
+++ backEnd/StudentDashboard/src/main/java/com/cornelius/StudentDashboard/service/CourseGradeService/CoursegradeService.java
@@ -0,0 +1,7 @@
+package com.cornelius.StudentDashboard.service.CourseGradeService;
+
+import com.cornelius.StudentDashboard.dto.course.CourseDTO;
+
+public interface CoursegradeService {
+    public CourseDTO getCourseGrades(String token);
+}
