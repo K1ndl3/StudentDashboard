e9ed5565c5567bc4547e6d6396311e3a1eaa475f
Huy Bui <HUYBUI02@csu.fullerton.edu>
2026-04-12 15:37:50 -0700

    finished with the global context dto

---
+++ backend/src/main/java/com/ScholarSync/backend/global_context_module/dto/GlobalContextDTO.java
@@ -0,0 +1,20 @@
+package com.ScholarSync.backend.global_context_module.dto;
+
+import java.util.List;
+
+import com.ScholarSync.backend.model_module.event.canvas_event.CanvasEvent;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GlobalContextDTO {
+    private String name;
+    private List<CanvasEvent> canvas_event;
+
+}
